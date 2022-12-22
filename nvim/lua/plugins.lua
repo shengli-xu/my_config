2eafaf7f809cf391da3379f094a193bb3597a6d6
GRAIL-Autonomy-Project <97678224+GRAIL-Autonomy-Project@users.noreply.github.com>
2022-12-22 16:16:39 -0500

    Update plugins.lua

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -83,10 +83,10 @@ return require('packer').startup(function(use)
   }
 
   -- Treesitter
-	use {
-		"nvim-treesitter/nvim-treesitter",
-		commit = "8e763332b7bf7b3a426fd8707b7f5aa85823a5ac",
-	}
+  use {
+    "nvim-treesitter/nvim-treesitter",
+    commit = "8e763332b7bf7b3a426fd8707b7f5aa85823a5ac",
+  }
 
   -- bufferline
   use {
