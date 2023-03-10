d346be518fdf1c16294e26be53b1f2bb06143cc4
GRAIL-Autonomy-Project <97678224+GRAIL-Autonomy-Project@users.noreply.github.com>
2023-03-10 11:06:12 -0500

    Update nvim_tree.lua

--- config/nvim/lua/nvim_tree.lua
+++ config/nvim/lua/nvim_tree.lua
@@ -10,6 +10,41 @@ end
 
 local tree_cb = nvim_tree_config.nvim_tree_callback
 
+-- Opening nvim-tree at neovim startup
+local function open_nvim_tree(data)
+
+  -- buffer is a directory
+  local directory = vim.fn.isdirectory(data.file) == 1
+
+  if not directory then
+    -- buffer is a real file on the disk
+    local real_file = vim.fn.filereadable(data.file) == 1
+
+    -- buffer is a [No Name]
+    local no_name = data.file == "" and vim.bo[data.buf].buftype == ""
+
+    if not real_file and not no_name then
+      return
+    end
+
+    --local folder = data.file:match("(.*[/\\])")
+    --vim.cmd.cd(folder)
+    
+    -- open the tree, find the file but don't focus it
+    require("nvim-tree.api").tree.toggle({ focus = false, find_file = true, update_root = true})
+    
+    return
+  end
+
+  -- change to the directory
+  vim.cmd.cd(data.file)
+
+  -- open the tree
+  require("nvim-tree.api").tree.open()
+end
+
+vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
+
 nvim_tree.setup {
   update_focused_file = {
     enable = true,
